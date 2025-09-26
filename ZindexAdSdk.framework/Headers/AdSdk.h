0747d652d17601546ea799533fe7646ddadc7625
ww <1564413823@qq.com>
2025-09-26 13:35:58 +0800

    Update ZindexAdSdk framework - 2025-09-26 13:35:58

---
+++ ZindexAdSdk.framework/Headers/AdSdk.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double AdSdkVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char AdSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
