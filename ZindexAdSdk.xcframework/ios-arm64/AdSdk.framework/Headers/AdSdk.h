c8c14d3b26aa00ec27a7207c1f39f1f997793e48
ww <1564413823@qq.com>
2025-09-30 16:18:56 +0800

    Release ZindexAdSdk v1.0.8 (Xcode构建)

---
+++ ZindexAdSdk.xcframework/ios-arm64/AdSdk.framework/Headers/AdSdk.h
@@ -0,0 +1,16 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by 王威 on 2025/8/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
